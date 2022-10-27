383cd9a1263c225386c21a399b08f5b9bcb8b6e1
leonard-porteria <porterialeonard@gmail.com>
2022-10-27 23:08:36 +0800

    rendering textbox, open and close from differenct compoennts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import './Header.css';
+import Textarea from './Textarea';
+
+import { useState } from 'react';
 
 const Header = () => {
+  const [isCreating, setIsCreating] = useState(false);
+
+  const handleAddNote = () => {
+    console.log('CREATING NEW NOTE');
+    setIsCreating(!isCreating);
+  };
+
   return (
-    <div className='Header'>
-      <h1>Note Application</h1>
-      <button>+ Add Note</button>
-    </div>
+    <>
+      <div className='Header'>
+        <h1>Note Application</h1>
+        <div className='buttons'>
+          <button className='filter'>⛬</button>
+          <button className='add-note' onClick={handleAddNote}>
+            + Add Note
+          </button>
+        </div>
+      </div>
+
+      {isCreating && <Textarea onCreatingChange={setIsCreating} />}
+    </>
   );
 };
 
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,17 +1,37 @@
-const Textarea = () => {
+import './Textarea.css';
+
+const Textarea = ({ onCreatingChange }) => {
   const handleChange = () => {
     console.log('change');
   };
 
+  const handleFocus = () => {
+    console.log('focusesd');
+  };
+
+  const handleOnCreatingChange = () => {
+    console.log('changed state');
+    onCreatingChange(false);
+  };
+
   return (
-    <textarea
-      defaultValue='Enter a new note...'
-      onChange={handleChange}
-      id=''
-      cols='50'
-      rows='10'
-      style={{ resize: 'none' }}
-    ></textarea>
+    <div className='Textarea'>
+      <div className='container'>
+        <input type='text' />
+        <textarea
+          placeholder='Enter a new note...'
+          onChange={handleChange}
+          onFocus={handleFocus}
+          cols='50'
+          rows='10'
+        ></textarea>
+        <div className='buttons'>
+          <button>color</button>
+          <button onClick={handleOnCreatingChange}>cancel</button>
+          <button>add</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
